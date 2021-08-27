c14691fdf970959f1a0f55c248f34971a6f91a34
labroid <orders@jacksonhome.net>
2021-08-27 18:57:54 -0400

    Updated to use built-in Path.read_bytes() over homegrown function

--- me_models.py
+++ me_models.py
@@ -4,23 +4,29 @@ from utils import config
 cfg = config()
 
 
-class Db_connect:
+class DbConnect:
     def __init__(self):
         me.connect(
             db=cfg.gphotos.database, alias=cfg.gphotos.collection, host=cfg.gphotos.host
         )
-        me.connect(db=cfg.local.database, alias=cfg.local.database, host=None)
         me.connect(
-            db=cfg.path_history.database,
-            alias=cfg.path_history.database,
-            host=cfg.path_history.host,
+            db=cfg.local.database,
+            alias=cfg.local.database,
+            host='labroidgce.mooo.com',
+            username='scott',
+            password='mongo14',
+            authentication_source='admin')
+        me.connect(
+            db=cfg.source_archive.database,
+            alias=cfg.source_archive.database,
+            host=cfg.source_archive.host,
         )
 
 
 class Gphoto(me.Document):  # TODO: Remove strict: false from metadata once db is clean
     gid = me.StringField()
     imageMediaMetadata = me.DictField()
-    md5Checksum = me.StringField(unique=True)
+    md5Checksum = me.StringField()
     mimeType = me.StringField()
     name = me.StringField()
     originalFilename = me.StringField()
@@ -36,36 +42,41 @@ class Gphoto(me.Document):  # TODO: Remove strict: false from metadata once db i
     }
 
 
-class Gphoto_state(me.Document):
+class GphotoState(me.Document):
     database_clean = me.BooleanField()
     start_token = me.StringField()
     meta = {"db_alias": cfg.gphotos.collection}
 
 
-# class Gphoto_parent(me.Document):  # Depricated?
-#     gid = me.StringField()
-#     mimeType = me.StringField()
-#     name = me.StringField()
-#     ownedByMe = me.BooleanField()
-#     parents = me.ListField()
-#     trashed = me.BooleanField()
-#     path = me.ListField()
-#     meta = {"db_alias": cfg.gphotos.collection}
+class Src_metadata(me.EmbeddedDocument):
+    cameraMake = me.StringField(default=None)
+    cameraModel = me.StringField(default=None)
+    creationTime = me.DateTimeField(default=None)
+    dateTimeOriginal = me.DateTimeField(default=None)
+    width = me.IntField(default=0)
+    height = me.IntField(default=0)
 
 
 class Photo(me.Document):
     src_path = me.StringField(default=None)
     size = me.IntField(default=None)
+    modifiedTime = me.DateTimeField(default=None)
     md5sum = me.StringField(default=None)
+    image_md5 = me.StringField(default=None)
+    src_metadata = me.EmbeddedDocumentField(Src_metadata)
     gid = me.StringField(default=None)
     in_gphotos = me.BooleanField(default=False)
+    in_process = me.BooleanField(default=False)
     mirrored = me.BooleanField(default=False)
     purged = me.BooleanField(default=False)
-    uploaded = me.BooleanField(default=False)
-    uploading = me.BooleanField(default=False)
-    upload_tries = me.IntField(default=0)
-    upload_elapsed = me.FloatField(default=0)
-    gphotos_path = me.ListField(default=None)
+    # uploaded = me.BooleanField(default=False)
+    # uploading = me.BooleanField(default=False)
+    # upload_tries = me.IntField(default=0)
+    # upload_elapsed = me.FloatField(default=0)
+    # gphotos_path = me.ListField(default=None)
+    src_filename = me.StringField(default=None)
+    takeout_path = me.StringField(default=None)
+    in_takeout = me.BooleanField(default=False)
     original_filename = me.StringField(default=None)
     gphoto_meta = me.DictField(default=None)  # TODO:  Delete this?
     meta = {"allow_inheritance": True}
@@ -75,7 +86,9 @@ class Queue(Photo):
     meta = {"db_alias": cfg.local.database}
 
 
-class Candidates(Photo):
+class Candidates(me.Document):
+    src_path = me.StringField(default=None)
+    md5sum = me.StringField(default=None)
     meta = {"db_alias": cfg.local.database}
 
 
@@ -91,12 +104,12 @@ class State(me.Document):
     mirror_root = me.StringField(default="")
     purge_ok = me.BooleanField(default=False)
     enqueue_ok = me.BooleanField(default=True)
-    status = me.StringField(default='')
+    upload_ok = me.BooleanField(default=False)
+    status = me.ListField(default=[])
     meta = {"db_alias": cfg.local.database}
 
 
-
-class SourceList(me.Document):
+class SourceArchive(me.Document):
     md5sum = me.StringField(default=None, unique=True)
     paths = me.ListField(default={})
-    meta = {"db_alias": cfg.path_history.database, "indexes": ["md5sum"]}
+    meta = {"db_alias": cfg.source_archive.database, "indexes": ["md5sum"]}
