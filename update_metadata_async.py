a8514efd1865a52f8058667f0db3a177d44fee4f
labroid <orders@jacksonhome.net>
2021-08-27 22:24:39 -0400

    Updated to use built-in Path.read_bytes() over homegrown function

--- update_metadata_async.py
+++ update_metadata_async.py
@@ -3,17 +3,19 @@ import io
 import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import time
+import subprocess
+from pathlib import Path
+import shlex
 
 from PIL import Image, ImageFile
 from loguru import logger
 import arrow
 
-
 from me_models import DbConnect, Src_metadata
 from me_models import Queue as PhotoQueue
 from utils import config, file_md5sum
 
-PARSED_EXIF = 'parsed_exif'
+PARSED_EXIF = "parsed_exif"
 
 cfg = config()
 logger.add("update_metadata.log", rotation="100 MB")
@@ -23,14 +25,21 @@ DbConnect()
 def main():
     job_queue = mp.Manager().Queue()
     logger.info("Filling queue")
+    n = 0
     for n, photo in enumerate(PhotoQueue.objects(md5sum=None)):
         job_queue.put(photo)
-    logger.info(f"Done filling queue with {n} items")
 
+    if not n:
+        logger.info("Done. Queue is empty - no files without MD5SUM")
+        return
+
+    logger.info(f"Done filling queue with {n + 1} items")
     with ProcessPoolExecutor(max_workers=3) as executor:
         futures = []
         for worker_num in range(3):
-            futures.append(executor.submit(update_metadata, queue=job_queue, worker_num=worker_num))
+            futures.append(
+                executor.submit(update_metadata, queue=job_queue, worker_num=worker_num)
+            )
             logger.info(f"Worker {worker_num} started")
         logger.info("Launched all workers and am back in main")
 
@@ -38,33 +47,46 @@ def main():
             logger.info(x)
 
 
-# noinspection PyMethodMayBeStatic
-def get_bytes(path):
-    with open(path, mode="rb") as fp:
-        return fp.read()
-
-
 def update_metadata(queue, worker_num):
-    logger.info(f"Made it in worker {worker_num}")
+    logger.info(f"Starting worker {worker_num}")
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     while not queue.empty():
         photo = queue.get()
-        ImageFile.LOAD_TRUNCATED_IMAGES = True
         logger.info(f"Worker {worker_num} getting metadata from: {photo.src_path}")
-        if photo.size > 1e9:
-            photo.md5sum = file_md5sum(photo.src_path)
-            photo.save()
-            next
-        photo_b = get_bytes(photo.src_path)
+        if photo.size > 100e6 or Path(photo.src_path).suffix.lower() in ['.mov', '.mp4', '.avi']:
+            print(f"Skipping {photo.src_path}")
+            # logger.info("Using shell MD5")
+            # md5_target = Path(photo.src_path)
+            # wsl_target = PurePosixPath('/mnt/') / md5_target.drive[0].lower() / PurePosixPath(*md5_target.parts[1:])
+            # command = f"wsl md5sum {str(wsl_target)}"
+            # command_string = f'"{command}"'
+            # commands = shlex.split(command)
+            # md5_shell = subprocess.run([*commands], capture_output=True)
+            # md5_shell = subprocess.run([command_string], capture_output=True)
+            # md5_shell = subprocess.run(['FCIV', '-md5', str(md5_target)])
+            # if md5_shell.returncode == 0:
+            #     md5sum = md5_shell.stdout
+            # try:
+            #     md5 = file_md5sum(photo.src_path)
+            #     photo.md5sum = md5
+            # except Exception as e:
+            #     logger.info(f"Skipping {photo.src_path} for failure: {e}")
+            #     continue
+            # with open(photo.src_path, 'rb') as f:
+            #     photo.md5sum = hashlib.md5(f.read()).hexdigest()
+            # photo.save()
+            continue
+        photo_b = Path(photo.src_path).read_bytes()
         photo.md5sum = hashlib.md5(photo_b).hexdigest()
         try:
             im = Image.open(io.BytesIO(photo_b))
         except OSError:
-            logger.info(f"Got file {photo.src_path}. Saving MD5 and moving on.")
+            logger.info(f"File {photo.src_path} won't process as image. Saving MD5 and moving on.")
             photo.save()
-            next
+            continue
         if PARSED_EXIF in im.info:
             src_metadata = Src_metadata()
-            src_metadata.cameraMake = im.info[PARSED_EXIF].get(0x010f, None)
+            src_metadata.cameraMake = im.info[PARSED_EXIF].get(0x010F, None)
             src_metadata.cameraModel = im.info[PARSED_EXIF].get(0x0110, None)
             creation_time = im.info[PARSED_EXIF].get(0x0132, None)
             if creation_time:
@@ -81,9 +103,13 @@ def update_metadata(queue, worker_num):
 
 def str_to_datetime(datestring):
     if datestring:
-        t = "".join([x for x in datestring if x in "0123456789:/ "])  # Strip extraneous characters and binary
+        t = "".join(
+            [x for x in datestring if x in "0123456789:/ "]
+        )  # Strip extraneous characters and binary
     try:
-        converted = arrow.get(t, ['YYYY:MM:DD HH:mm:ss', 'YYYY:MM:DD HH:mm', 'DD/MM/YYYY'])
+        converted = arrow.get(
+            t, ["YYYY:MM:DD HH:mm:ss", "YYYY:MM:DD HH:mm", "DD/MM/YYYY"]
+        )
         return converted.datetime
     except ValueError as e:
         logger.warning(f"Value error: Datestring: {t} Binary equiv: {t.encode()} {e}")
